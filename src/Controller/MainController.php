c19a61367ab3789acb1b6a4e2efb29bdf6ba6192
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2025-08-05 15:03:22 +0200

    Correction tp5 partie 1

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Course;
+use App\Entity\Wish;
+use App\Form\CourseType;
+use App\Form\WishType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,5 +24,4 @@ class MainController extends AbstractController
     public function aboutUs() : Response {
         return $this->render('main/about_us.html.twig');
     }
-
 }
--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -34,6 +37,32 @@ final class WishController extends AbstractController
         ]);
     }
 
+    #[Route('/wish-create', name: 'wish_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $wish = new Wish();
+
+        $form = $this->createForm(WishType::class, $wish);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Publié par défaut
+            $wish->setIsPublished(true);
+            // Date = Now
+            $wish->setDateCreated(new \DateTimeImmutable("now"));
+
+            $em->persist($wish);
+            $em->flush();
+
+            $this->addFlash("success", "Le voeux a été crée avec succès");
+
+            return $this->redirectToRoute('wish_detail', ['id' => $wish->getId()]);
+        }
+
+        return $this->render('wish/create.html.twig', [
+            'wishForm' => $form->createView(),
+        ]);
+    }
 
 
 }
